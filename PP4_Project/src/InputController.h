6b1961a8aec5d8db58eed2ee7c7c43ae1d9ea31f
Derrick Greg Ramirez <dgramirez@student.fullsail.edu>
2019-05-31 00:24:13 -0400

    Forgot to add these files

---
+++ PP4_Project/src/InputController.h
@@ -0,0 +1,21 @@
+#ifndef INPUT_CONTROLLEr_H
+#define INPUT_CONTROLLEr_H
+
+struct InputController
+{
+	static inline bool m_forward	= false;
+	static inline bool m_back		= false;
+	static inline bool m_left		= false;
+	static inline bool m_right		= false;
+	static inline bool m_up			= false;
+	static inline bool m_down		= false;
+
+	static inline bool r_posPitch	= false;
+	static inline bool r_negPitch	= false;
+	static inline bool r_posRoll	= false;
+	static inline bool r_negRoll	= false;
+	static inline bool r_posYaw		= false;
+	static inline bool r_negYaw		= false;
+};
+
+#endif
